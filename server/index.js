28229ed8165dd244e5b36177e534deaf3078e35e
Ahsan <sajibnet@gmail.com>
2024-06-04 01:55:45 +0200

    Add Docker containers configuration files

--- server/index.js
+++ server/index.js
@@ -4,16 +4,27 @@ const dotenv = require('dotenv').config()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const path = require('path');
 
 const app = express()
 
-app.use(cors(
-    {
-        origin: 'http://localhost:5173',
-        method: ["POST","GET"],
-        credentials: true
-    }
-))
+// CORS options
+const corsOptions = {
+    origin: ['http://localhost', 'http://localhost:80', 'http://localhost:3000'],  // Allowed origins
+    credentials: true,  // Allow cookies to be sent
+    optionsSuccessStatus: 200 // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
+// app.use(cors(
+//     {
+//         origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+//         method: ["POST","GET"],
+//         credentials: true
+//     }
+// ))
+
 
 // ---Middlewares------------
 app.use(express.json());
@@ -21,10 +32,6 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
 const partRoutes = require('./routes/partRoutes');
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/Workshop_database")
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
 
 //routes paths
 app.use('/',require('./routes/authRoutes'))
@@ -34,6 +41,22 @@ app.use('/',require('./routes/availabilityRoutes'))
 app.use(partRoutes);
 
 
+//--------------
+//when frontend build (dist) is created to serve static frontend files for production
+// Serve static files from the React app build directory
+//only works on localmachine when both backend and frontend running on the same instance
+app.use(express.static(path.join(__dirname, '../client/dist')));
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
-const port = 80;
+//----
+//mongoose.connect("mongodb://127.0.0.1:27017/Workshop_database")
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+
+const port = process.env.PORT || 5000;
 app.listen(port,()=>console.log(`Server is running on port ${port}`))
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,15 +4,8 @@ const router = express.Router();
 const cors = require('cors');
 const { test, registerUser, loginUser, getProfile , doLogout } = require('../controllers/authController');
 
-// middleware
-// router.use(
-//     cors({
-//         credentials:true,
-//         origin: 'http://localhost:5173'
-//     })
-// )
 
-router.get('/',test);
+router.get('/');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', getProfile);
